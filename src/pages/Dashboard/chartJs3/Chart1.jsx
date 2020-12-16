db8f9858869ccc3253281115f00984b1c4c4d59a
matheusjuan1 <61558499+matheusjuan1@users.noreply.github.com>
2020-12-16 10:18:58 -0300

    Update Chart1.jsx

--- src/pages/Dashboard/chartJs3/Chart1.jsx
+++ src/pages/Dashboard/chartJs3/Chart1.jsx
@@ -2,58 +2,53 @@ import React, { useEffect, useState } from "react";
 import { HorizontalBar } from "react-chartjs-2";
 import api from "../../../services/api";
 
-const Chart1 = ({idgestor}) => {
+const Chart1 = ({ idgestor }) => {
   const [chartData, setChartData] = useState({});
 
-  const chart = () => {
-    let nome1 = [];
-    let quantidade1 = [];
-    api
-      .get(`/dash/sites/${idgestor}`)
-      .then((res) => {
-        for (const dataObj of res.data.recordset) {
-          quantidade1.push(parseInt(dataObj.quantidade));
-          nome1.push(dataObj.titulo);
-        }
-        setChartData({
-          labels: nome1,
-          datasets: [
-            {
-              label: "Sites",
-              data: quantidade1,
-              backgroundColor: [
-                "#7BAAC4b1",
-                "#FF3D22b1",
-                "#7BAAC4b1",
-                "#FF3D22b1",
-                "#7BAAC4b1",
-                "#FF3D22b1",
-              ],
-              borderColor: [
-                "#7BAAC4",
-                "#FF3D22",
-                "#7BAAC4",
-                "#FF3D22",
-                "#7BAAC4",
-                "#FF3D22",
-              ],
-              borderWidth: 3,
-            },
-          ],
+  useEffect(() => {
+    function chart() {
+      let nome1 = [];
+      let quantidade1 = [];
+      api
+        .get(`/dash/sites/${idgestor}`)
+        .then((res) => {
+          for (const dataObj of res.data.recordset) {
+            quantidade1.push(parseInt(dataObj.quantidade));
+            nome1.push(dataObj.titulo);
+          }
+          setChartData({
+            labels: nome1,
+            datasets: [
+              {
+                label: "Sites",
+                data: quantidade1,
+                backgroundColor: [
+                  "#7BAAC4b1",
+                  "#FF3D22b1",
+                  "#7BAAC4b1",
+                  "#FF3D22b1",
+                  "#7BAAC4b1",
+                  "#FF3D22b1",
+                ],
+                borderColor: [
+                  "#7BAAC4",
+                  "#FF3D22",
+                  "#7BAAC4",
+                  "#FF3D22",
+                  "#7BAAC4",
+                  "#FF3D22",
+                ],
+                borderWidth: 3,
+              },
+            ],
+          });
+        })
+        .catch((err) => {
+          console.log(err);
         });
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  };
-
-  //useEffect(() => {
-  //  chart();
-  //}, [chart]);
-
-  React.useCallback(() => {
+    }
     chart();
-  }, [chart])
+  }, [idgestor]);
 
   return (
     <div>
